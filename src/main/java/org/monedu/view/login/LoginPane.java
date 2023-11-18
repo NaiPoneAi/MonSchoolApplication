54af06169b9a5e66bde379d47a9063b0809f1ddb
Nai Pone Ai <zmin07@gmail.com>
2023-11-18 20:21:33 +0630

    Commit

--- src/main/java/org/monedu/view/login/LoginPane.java
+++ src/main/java/org/monedu/view/login/LoginPane.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import net.rgielen.fxweaver.core.FxmlView;
-import org.monedu.view.login.LoginPaneManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
